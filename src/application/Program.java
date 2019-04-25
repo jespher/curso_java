033c70c9ee168c363eadcb1b1e53fb6f41e44e0b
Jefferson <luis.88.rasini@gmail.com>
2019-04-25 13:07:27 -0300

    Solucao sem o uso da orientação a objetos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,43 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        double xA, xB, xC;
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+        double xA, xB, xC, yA, yB, yC;
+
+        System.out.println("Digite as medidas do triângulo X: ");
+        xA = scan.nextDouble();
+        xB = scan.nextDouble();
+        xC = scan.nextDouble();
+
+        System.out.println("Digite as medidas do triângulo Y: ");
+        yA = scan.nextDouble();
+        yB = scan.nextDouble();
+        yC = scan.nextDouble();
+
+        double p = (xA + xB + xC) / 2.0;
+        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+        p = (yA + yB + yC) / 2.0;
+        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+        System.out.printf("Área do triângulo X: %.4f%n", areaX);
+
+        System.out.printf("Área do triângulo Y: %.4f%n", areaY);
+
+        if (areaX > areaY) {
+            System.out.println("Maior area é do X");
+        }
+        else {
+            System.out.println("Maior area é do Y");
+        }
+
+        scan.close();
     }
 }
