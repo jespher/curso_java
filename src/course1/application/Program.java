61e0d4504c4c10dd69a608c6b1e54ba1731a4e17
Jefferson <luis.88.rasini@gmail.com>
2019-04-25 14:51:11 -0300

    Finalizando programa

--- src/course1/application/Program.java
+++ src/course1/application/Program.java
@@ -21,6 +21,23 @@ public class Program {
         System.out.print("Quantidade em estoque: ");
         product.quantidade = scanner.nextInt();
 
+        System.out.println();
+        System.out.println(product);
+
+        System.out.println();
+        System.out.println("Entre com o número de produtos para ser adicionado ao estoque: ");
+        int quantidade = scanner.nextInt();
+        product.adicionarProdutos(quantidade);
+
+        System.out.println();
+        System.out.println(product);
+
+        System.out.println();
+        System.out.println("Entre com o número de produtos para ser removido do estoque: ");
+        int quantidade2 = scanner.nextInt();
+        product.removerProdutos(quantidade2);
+
+        System.out.println();
         System.out.println(product);
 
         scanner.close();
